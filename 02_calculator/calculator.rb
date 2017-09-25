c8b5644e09bf38104f0b51b6f2b019cf67aa20db
Brigitte Santos <santosbrigitte@gmail.com>
2017-09-24 20:26:31 -0700

    Finish exercise 02_calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,41 @@
 #write your code here
+def add(x,y)
+  x+y
+end
+
+def subtract(x,y)
+  x-y
+end
+
+def sum(arr)
+  sum = 0
+  arr.each do |x|
+    sum = sum + x
+  end
+  sum
+end
+
+def multiply(x,y,*args)
+  product = x*y
+  args.each do |number|
+    product = product*number
+  end
+  product
+end
+
+def power(x,y)
+  x**y
+end
+
+def factorial(x)
+  product = 1
+  if x==0
+    product = 0
+  else
+    while x != 0
+      product = product*x
+      x = x-1
+    end
+  end
+  product
+end
